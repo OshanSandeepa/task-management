5b97b1d3dc3f61b23d24a64b80eb0d464e6a02ef
OshanSandeepa <87821648+OshanSandeepa@users.noreply.github.com>
2024-01-15 13:03:32 +0530

    basic add state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [tasks,setTasks]=useState({
+      title:" ",
+      date: " ",
+      status : " ",
+      prioriy : " ",
+
+
+    });
+
+    console.log(tasks);
+
+    const handleOnChange=(e)=>{
+       setTasks({...tasks,[e.target.name]:e.target.value });
+    };
+
+
   return (
-    <div className="App">
-      Test 
-    </div>
+
+    //task
+    //date
+    //status
+    //priority
+
+    <div className="App" >
+      <div className="h-screen w-auto bg-red-300">
+        <div className="flex items-center  justify-between">
+          <div className="flex flex-col">      
+                <label>task</label>
+        <input 
+               name="title" 
+               onChange={  (e)=>handleOnChange(e) }
+               value={tasks.title}    
+               placeholder="task"
+                       />
+
+           </div>
+          </div>
+
+
+
+
+        </div>
+
+      
+      
+      </div>
   );
 }
 
