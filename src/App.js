268794147d9a33123daf8bfcc20ccf23c1c5c2d9
OshanSandeepa <87821648+OshanSandeepa@users.noreply.github.com>
2024-01-16 10:54:50 +0530

    Add task

--- src/App.js
+++ src/App.js
@@ -2,52 +2,95 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [tasks,setTasks]=useState({
-      title:" ",
-      date: " ",
-      status : " ",
-      prioriy : " ",
+  const [tasks, setTasks] = useState({
+    title: "",
+    date: "",
+    status: "",
+    priority: false,
+  });
+  const [taskList, setTaskList] = useState([]);
 
+  console.log(tasks);
 
-    });
+  const handleOnChange = (e) => {
+    if (e.target.name === "priority") {
+      setTasks({ ...tasks, [e.target.name]: e.target.checked });
+    } else {
+      setTasks({ ...tasks, [e.target.name]: e.target.value });
+    }
+  };
 
-    console.log(tasks);
+  const handleAdd = () => {
+    // const sampleList = [...taskList];
+    // sampleList.push(tasks);
+    // setTaskList(sampleList);
+    // setTasks({
+    //   title: "",
+    //   date: "",
+    //   status: "",
+    //   priority: false,
+    // });
 
-    const handleOnChange=(e)=>{
-       setTasks({...tasks,[e.target.name]:e.target.value });
-    };
-
+    setTaskList([...taskList,tasks]);
+  };
 
   return (
-
-    //task
-    //date
-    //status
-    //priority
-
-    <div className="App" >
-      <div className="h-screen w-auto bg-red-300">
-        <div className="flex items-center  justify-between">
-          <div className="flex flex-col">      
-                <label>task</label>
-        <input 
-               name="title" 
-               onChange={  (e)=>handleOnChange(e) }
-               value={tasks.title}    
-               placeholder="task"
-                       />
-
-           </div>
+    <div className="App">
+      <div className="h-screen w-auto px-6 bg-blue-200">
+        <div className="flex items-center gap-8">
+          <div className="flex flex-col">
+            <label>Task</label>
+            <input
+              name="title"
+              type="text"
+              onChange={(e) => handleOnChange(e)}
+              value={tasks.title}
+              placeholder="Task"
+            />
           </div>
 
+          <div className="flex flex-col">
+            <label>Date</label>
+            <input
+              name="date"
+              type="date"
+              onChange={(e) => handleOnChange(e)}
+              value={tasks.date}
+            />
+          </div>
 
+          <div className="flex flex-col">
+            <label>Priority</label>
+            <input
+              name="priority"
+              type="checkbox"
+              onChange={(e) => handleOnChange(e)}
+              checked={tasks.priority}
+            />
+          </div>
 
+          <div>
+            <button
+              className="bg-blue-400 px-4 py-2 min-w-[6rem] rounded"
+              onClick={handleAdd}
+            >
+              Add
+            </button>
+          </div>
 
+          <div className="mt-4">
+            {taskList.map((task, idx) => (
+              <div key={idx}>
+                <div>{task.title}</div>
+                <div>{task.date}</div>
+                <div>{task.priority.toString()}</div>
+                <hr />
+              </div>
+            ))}
+          </div>
         </div>
-
-      
-      
       </div>
+    </div>
   );
 }
 
